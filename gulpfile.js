6798c80cf3e8fc6f035be6fbcbf8240189f2c3c7
AlexShelter <allinshelter@gmail.com>
2020-03-17 21:50:05 +0400

    feat: add normalize, grid, variabless, inteface,  header styles in template folder

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+
+
+// Static server
+function bs() {
+  browserSync.init({
+      server: {
+          baseDir: "./src"
+      }
+  });
+   watch("./src/**/*.html").on('change', browserSync.reload);
+   watch("./src/sass/**/*.sass",serveSass); 
+   watch("./src/js/**/*.js").on('change', browserSync.reload);
+   watch("./src/css/**/*.css").on('change', browserSync.reload);
+};
+
+function serveSass() {
+  return src("./src/sass/**/*.sass")
+      .pipe(sass())
+      .pipe(dest("./src/css"))
+      .pipe(browserSync.stream());
+};
+
+function minCss(done) {
+  src('./src/css/**/*.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(dest('./dist/css'));
+      done();
+};
+
+exports.serve = bs;
+exports.minCss = minCss;
+
